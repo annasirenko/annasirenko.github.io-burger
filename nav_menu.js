ee81692dad7c499f23b2277a99320ed2b038d71b
Anna Sirenko <otherjam@gmail.com>
2019-09-01 14:54:15 +0300

    js fixed

--- nav_menu.js
+++ nav_menu.js
@@ -1,5 +1,5 @@
-let hamburger = document.querySelector('.hamburger');
-let fullscreen = document.querySelector('.fullscreen');
+const hamburger = document.querySelector('.hamburger');
+const fullscreen = document.querySelector('.fullscreen');
 let menuItems = document.querySelectorAll('.vertical-menu-list__link');
 
 function openHamburgerMenu(){
@@ -17,38 +17,42 @@ function closeHamburgerMenu(){
 hamburger.addEventListener('click', function(e) {
   e.preventDefault();
    if(hamburger.classList.contains('is-active')){
+    //  fullscreen.classList.add('fadeOut');
+    //  setTimeout(() => {
       closeHamburgerMenu();
+      // fullscreen.classList.remove('fadeOut');
+    //  }, 400);
+      
    } else{
       openHamburgerMenu();
    }
    
   });
 
-  fullscreen.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log(e.target);
+  // fullscreen.addEventListener('click', function(e){
+  //   e.preventDefault();
+  //   console.log(e.target);
     
-    if(e.target.classList.contains('.vertical-menu-list__link')){
-      closeHamburgerMenu();
-    }
-
-  });
-
-
-
-  // for (let i=0; i<menuItems.length; i++){
-  //   const menuItem = menuItems[i];
-  //   menuItem.addEventListener('click', function(e) {
-  //     e.preventDefault();
+  //   if(e.target.classList.contains('.vertical-menu-list__link')){
   //     closeHamburgerMenu();
+  //   }
 
-  //   });
-  // }
+  // });
+
+
+
+  for (let i=0; i<menuItems.length; i++){
+    const menuItem = menuItems[i];
+    menuItem.addEventListener('click', function(e) {
+      e.preventDefault();
+      closeHamburgerMenu();
+
+    });
+  }
 
 
 // let fullscreenExit = document.querySelector('.hamburger.is-active');
 // fullscreenExit.addEventListener('click', function() { 
 //   fullscreen.style.right = '-100%';
 // });
-
 
